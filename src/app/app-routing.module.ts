f57277807666f582d1d1a6cd8ffe1bb6d6f55efe
Makwale <makwale.em@gmail.com>
2021-02-11 05:15:58 -0800

    Emmanuels commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { MainPage } from './pages/main/main.page';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
     redirectTo: 'main',
@@ -13,44 +11,53 @@ const routes: Routes = [
   },
   {
     path: 'main',
-    loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
+    component: MainPage,
+    children: [  {
+              path: 'home',
+              loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+              {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+              },
+              {
+                path: 'featuredcourses',
+                loadChildren: () => import('./pages/featuredcourses/featuredcourses.module').then( m => m.FeaturedcoursesPageModule)
+              },
+              {
+                path: 'categories',
+                loadChildren: () => import('./pages/categories/categories.module').then( m => m.CategoriesPageModule)
+              },
+              {
+                path: 'account',
+                loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
+              },
+              {
+                path: 'popularinstructors',
+                loadChildren: () => import('./pages/popularinstructors/popularinstructors.module').then( m => m.PopularinstructorsPageModule)
+              },
+              {
+                path: 'about',
+                loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+              },
+              {
+                path: 'contacts',
+                loadChildren: () => import('./pages/contacts/contacts.module').then( m => m.ContactsPageModule)
+              },
+              {
+                path: 'courseinrolled',
+                loadChildren: () => import('./pages/courseinrolled/courseinrolled.module').then( m => m.CourseinrolledPageModule)
+              },
+              {
+                path: 'latestcourses',
+                loadChildren: () => import('./pages/latestcourses/latestcourses.module').then( m => m.LatestcoursesPageModule)
+              },
+              {
+                path: 'popularcourses',
+                loadChildren: () => import('./pages/popularcourses/popularcourses.module').then( m => m.PopularcoursesPageModule)
+              },]
   },
-  {
-    path: 'featuredcourses',
-    loadChildren: () => import('./pages/featuredcourses/featuredcourses.module').then( m => m.FeaturedcoursesPageModule)
-  },
-  {
-    path: 'categories',
-    loadChildren: () => import('./pages/categories/categories.module').then( m => m.CategoriesPageModule)
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
-  },
-  {
-    path: 'popularinstructors',
-    loadChildren: () => import('./pages/popularinstructors/popularinstructors.module').then( m => m.PopularinstructorsPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'contacts',
-    loadChildren: () => import('./pages/contacts/contacts.module').then( m => m.ContactsPageModule)
-  },
-  {
-    path: 'courseinrolled',
-    loadChildren: () => import('./pages/courseinrolled/courseinrolled.module').then( m => m.CourseinrolledPageModule)
-  },
-  {
-    path: 'latestcourses',
-    loadChildren: () => import('./pages/latestcourses/latestcourses.module').then( m => m.LatestcoursesPageModule)
-  },
-  {
-    path: 'popularcourses',
-    loadChildren: () => import('./pages/popularcourses/popularcourses.module').then( m => m.PopularcoursesPageModule)
-  },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPage } from './pages/main/main.page';
+
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainPage],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
