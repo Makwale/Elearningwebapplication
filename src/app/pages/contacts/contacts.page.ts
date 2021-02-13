cba5b23e7cbaba354c44038de14016af04a6d088
Sinah62 <mosimasinah62@gmail.com>
2021-02-13 10:36:33 -0800

    'work'

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,17 +1,12 @@
 import { Component, OnInit,OnDestroy } from '@angular/core';
 import { ModalController, AlertController} from '@ionic/angular';
-import * as Leaflet from 'leaflet';
-import { antPath } from 'leaflet-ant-path';
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.page.html',
   styleUrls: ['./contacts.page.scss'],
 })
-export class ContactsPage implements OnInit , OnDestroy {
-
-  map: Leaflet.Map;
-  
-  
+export class ContactsPage implements OnInit {
 
   constructor( private modalCtrl: ModalController, private alertCtrl: AlertController) { }
 
@@ -29,36 +24,6 @@ export class ContactsPage implements OnInit , OnDestroy {
     });
   }
 
-  ionViewDidEnter() { this.leafletMap(); }
-
-  leafletMap() {
-
-
-    this.map = Leaflet.map('mapId').setView([-26.270760, 28.112268],10);
-    Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    
-    }).addTo(this.map);
- 
-    var b = new Leaflet.Marker([-26.21197, 28.25958]); 
-    var jbrg = new Leaflet.Marker([-26.253811, 27.974689]); 
-    var a = new Leaflet.Marker([-26.267222, 28.121944]); 
-
-    b.addTo(this.map);
-    jbrg.addTo(this.map);
-    a.addTo(this.map);
-
-
-    
+  
   }
   
-   /** Remove map when we have multiple map object */
-   ngOnDestroy() {
-    this.map.remove();
-  }
-  
-  ionViewWillLeave() {
-    this.map.remove();
-  }
-
-}
