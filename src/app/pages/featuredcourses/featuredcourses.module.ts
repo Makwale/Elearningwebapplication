263c6195bfcd7deb8ccf74b148f87b83fa9362a7
703Pvrtcx <kasaokapapkies@gmail.com>
2021-02-14 13:53:48 +0200

    Fixed Featured and Latest Courses, how it works as a footer

--- src/app/pages/featuredcourses/featuredcourses.module.ts
+++ src/app/pages/featuredcourses/featuredcourses.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FeaturedcoursesPageRoutingModule } from './featuredcourses-routing.module';
 
 import { FeaturedcoursesPage } from './featuredcourses.page';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FeaturedcoursesPageRoutingModule
+    FeaturedcoursesPageRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatStepperModule
+
   ],
   declarations: [FeaturedcoursesPage]
 })
--- src/app/pages/latestcourses/latestcourses.module.ts
+++ src/app/pages/latestcourses/latestcourses.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LatestcoursesPageRoutingModule } from './latestcourses-routing.module';
 
 import { LatestcoursesPage } from './latestcourses.page';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LatestcoursesPageRoutingModule
+    LatestcoursesPageRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatStepperModule
   ],
   declarations: [LatestcoursesPage]
 })
