606092ff51edb7b00c378fd002736361c51aba02
703Pvrtcx <kasaokapapkies@gmail.com>
2021-02-21 13:12:55 +0200

    Added validation on signup and login page.....

--- src/app/models/course.ts
+++
@@ -1,8 +0,0 @@
-class Course{
-    name: string;
-    image: string;
-    gradNo: number;
-    desc: string;
-    instructor: string;
-    price: number;  
-}
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -6,15 +6,13 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class DatabaseService {
-
-  constructor(private asf: AngularFirestore, private afa: AngularFireAuth, private router: Router) { }
-
-
+  constructor(private asf: AngularFirestore,
+     private afa: AngularFireAuth, 
+     private router: Router) { }
   //The method to create student account
   createUserAccount(name: string, surname: string, gender: string, phone: string, email: string, password: string){
     this.afa.createUserWithEmailAndPassword( email, password).then( userCredentials => {
       let id = userCredentials.user.uid;
-
       this.asf.collection("Student").doc(id).set({
         firstname: name,
         lastname: surname,
@@ -32,15 +30,44 @@ export class DatabaseService {
     }).catch(error => {
       alert(error);
     })
-  
   }
-
-
   signinWithEmailAndPassword(email: string, password: string){
       this.afa.signInWithEmailAndPassword(email, password).then( res => {
         this.router.navigateByUrl("home");
       }).catch(error =>{
         alert(error)
       });
+  }
+  // Login user with email/password
+  SignIn(email, password) {    
+    return this.afa.signInWithEmailAndPassword(email, password)
+    .then(res => {
+      this.router.navigateByUrl("home");
+       console.log( 'Signin success');
+    }).catch(error =>{
+      alert(error)
+    });
+  }
+  // Register user with email/password
+  RegisterUser(name: string, surname: string, gender: string, phone: string, email: string, password: string) {
+    //return this.afa.createUserWithEmailAndPassword(email, password);
+    return this.afa.createUserWithEmailAndPassword( email, password).then( userCredentials => {
+      let id = userCredentials.user.uid;
+      this.asf.collection("Student").doc(id).set({
+        firstname: name,
+        lastname: surname,
+        gender: gender,
+        phone: phone,
+        email: email,
+      }).then( res => {
+        alert("Your account is succesfully created");
+      }).catch( error => {
+        alert(error)
+      }).catch( error => {
+        alert(error)
+      })
+    }).catch(error => {
+      alert(error);
+    })
   }
 }
