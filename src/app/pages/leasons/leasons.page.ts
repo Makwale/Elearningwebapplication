79a2ac2ff3eb7b219a63ebd4168c704090cbd424
Makwale <makwale.em@gmail.com>
2021-02-26 04:54:36 -0800

    Lesson videos from database

--- src/app/pages/leasons/leasons.page.ts
+++ src/app/pages/leasons/leasons.page.ts
@@ -1,6 +1,7 @@
 import { ConstantPool } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Lesson } from 'src/app/Model/lesson.mode';
 import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
@@ -11,21 +12,36 @@ import { DatabaseService } from 'src/app/services/database.service';
 export class LeasonsPage implements OnInit {
 selectTabs ="recent";
 videoURL;
- lessonList;
+ lessonList: Lesson[];
   constructor(public activageRoute: ActivatedRoute,public dbs: DatabaseService) { }
 
   ngOnInit() {
     
     this.activageRoute.queryParams.subscribe(data =>{
-
+      this.lessonList = [];
        this.lessonList = this.dbs.lessonsList.filter( lesson => lesson.course_id == data["course_id"]);
-       console.log(this.lessonList);
-
+       //console.log(data["course_id"]);
+       this.sort();
+      
     })
   }
 
   play(videoURL){
     this.videoURL = videoURL;
+  }
+
+  sort(){
+    let tempvar: Lesson;
+    for(let i = 0; i < this.lessonList.length; i++){
+      
+      for(let k = 0 ; k < this.lessonList.length; k++){
+        if(this.lessonList[i].number < this.lessonList[k].number){
+          tempvar = this.lessonList[k];
+          this.lessonList[k] = this.lessonList[i];
+          this.lessonList[i] = tempvar;
+        }
+      }
+    }
   }
 
 }
