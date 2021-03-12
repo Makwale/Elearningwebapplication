5fc47356a8a68990b45f4a230ee874efa331d666
Noxolo22 <sinoxolonoxolo77@gmail.com>
2021-03-12 14:00:18 +0200

    timer

--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -11,6 +11,20 @@ import { QuizserviceService } from 'src/app/services/quizservice.service';
   styleUrls: ['./quiz.page.scss'],
 })
 export class QuizPage implements OnInit {
+  [x: string]: any;
+
+
+  timeInSeconds
+  time
+  runTimer
+  hasStarted
+  hasFinished
+  remainingTime
+  displayTime
+
+
+
+
 
   quiz: Quiz;
 
@@ -34,36 +48,40 @@ export class QuizPage implements OnInit {
   ngOnInit() {
 
     this.activatedRoute.queryParams.subscribe(data => {
-     
+
       this.dbs.getQuiz(data["lesson_id"]).subscribe( data =>{
         data.forEach(quizdata => {
           let tempvar = quizdata.payload.doc.data();
-  
+
           this.quiz = new Quiz(quizdata.payload.doc.id, tempvar["lesson_id"], tempvar["total_marks"], tempvar["duration"], tempvar["questions"])
-  
+
           this.duration = new Date();
           this.duration.setMinutes(tempvar["duration"]);
         })
       })
     })
 
-    
+    this.initTimer();
+    this.startTimer();
+    this.timeOut();
+
+
   }
 
-  
+
   done(){
-   
+
     this.qs.questionResults = this.questionResults;
 
     this.qs.quiz = this.quiz;
 
     this.router.navigateByUrl("quizresults");
-    
+
   }
 
   save(question, st_answer){
-    
-    
+
+
     this.questionResults.push(new QuestionResults(question.question, question.answer , question.marks ,st_answer ))
 
   }
@@ -76,5 +94,72 @@ export class QuizPage implements OnInit {
       //alert("Hello world");
     },1000)
   }
+
+  initTimer() {
+
+   if (!this.timeInSeconds) {
+     this.timeInSeconds = 1800;
+   }
+
+   this.time = this.timeInSeconds;
+   this.runTimer = false;
+   this.hasStarted = false;
+   this.hasFinished = false;
+   this.remainingTime = this.timeInSeconds;
+
+   this.displayTime = this.getSecondsAsDigitalClock(this.remainingTime);
+  }
+
+  startTimer() {
+    this.runTimer = true;
+   this.hasStarted = true;
+   this.timerTick();
+  }
+
+  pauseTimer() {
+   this.runTimer = false;
+  }
+
+
+timeOut(){
+  if(this.displayTime <= 1 ){
+    window.alert('We are done');
+    //this.done();
+  }
+}
+
+  resumeTimer() {
+   this.startTimer();
+  }
+
+  timerTick() {
+   setTimeout(() => {
+
+     if (!this.runTimer) { return; }
+     this.remainingTime--;
+     this.displayTime = this.getSecondsAsDigitalClock(this.remainingTime);
+     if (this.remainingTime > 0) {
+       this.timerTick();
+     }
+     else {
+       this.hasFinished = true;
+     }
+   }, 1000);
+  }
+
+  getSecondsAsDigitalClock(inputSeconds: number) {
+   var sec_num = parseInt(inputSeconds.toString(), 10);
+   var hours = Math.floor(sec_num / 3600);
+   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+   var seconds = sec_num - (hours * 3600) - (minutes * 60);
+   var hoursString = '';
+   var minutesString = '';
+   var secondsString = '';
+   hoursString = (hours < 10) ? "0" + hours : hours.toString();
+   minutesString = (minutes < 10) ? "0" + minutes : minutes.toString();
+   secondsString = (seconds < 10) ? "0" + seconds : seconds.toString();
+   return hoursString + ':' + minutesString + ':' + secondsString;
+  }
+
 
 }
