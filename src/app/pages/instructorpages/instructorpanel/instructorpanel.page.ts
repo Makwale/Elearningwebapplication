db953c6461c4943f5af7169e846260432d18f5e0
Makwale <makwale.em@gmail.com>
2021-03-14 12:08:28 +0100

    Instructor pages

---
+++ src/app/pages/instructorpages/instructorpanel/instructorpanel.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instructorpanel',
+  templateUrl: './instructorpanel.page.html',
+  styleUrls: ['./instructorpanel.page.scss'],
+})
+export class InstructorpanelPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
