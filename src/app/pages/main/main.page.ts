4d996b5a8d20451081d0e77d11fd979b4b8746ae
Makwale <makwale.em@gmail.com>
2021-03-18 14:54:24 +0100

    Search is working

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -35,7 +35,7 @@ export class MainPage implements OnInit {
       if (user) {
         this.afs.collection("Student").doc(user.uid).valueChanges().subscribe(data =>{
           // set student data
-          let student = new Student(user.uid,data["firstname"], data["lastname"], data["phone"],data["gender"], data["email"]);
+          let student = new Student(user.uid,data["firstname"], data["lastname"], data["phone"],data["gender"], data["email"], data["imgURL"]);
           //create account object that has sign state and student object
           this.userAccount = new Account(true, student);
           //set Account service to keep account object
